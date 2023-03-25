a7fd4534095c14e3d87ddf8e987502d6709cc85a
Valeriya <val.shelukhina@gmail.com>
2023-03-26 02:03:37 +0300

    add sorting

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,14 +1,15 @@
-import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
 import cn from 'classnames';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useDrop } from 'react-dnd';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuid } from 'uuid';
-import { ADD_INGREDIENT } from '../../services/reducers/burger-constructor';
+import { ADD_INGREDIENT, SORT_INGREDIENTS } from '../../services/reducers/burger-constructor';
 import { RootState } from '../../store';
 import { IIngredient } from '../../types/ingredientTypes';
 import Modal from '../modal/modal';
 import OrderDetails from '../order-details/order-details';
+import { DraggableItem } from './draggable-item';
 import OrderConfirmation from './order-confirmation/order-confirmation';
 
 import styles from './style.module.css';
@@ -51,6 +52,13 @@ const BurgerConstructor = ({ className }: IProps) => {
     }
   };
 
+  const moveItem = useCallback(
+    (dragIndex: number, hoverIndex: number) => {
+      dispatch(SORT_INGREDIENTS({ from: dragIndex, to: hoverIndex }));
+    },
+    [dispatch]
+  );
+
   return (
     <section className={cn(className, 'pt-100')} ref={dropTargetRef}>
       {!bun || (!bun && ingredientList.length === 0) ? (
@@ -67,11 +75,16 @@ const BurgerConstructor = ({ className }: IProps) => {
             />
           </div>
           <div className={cn(styles.scrollBlock, 'custom-scroll pl-4 pr-4')}>
-            {ingredientList.map(el => (
-              <div key={uuid()} className={styles.availableConstructorItem}>
-                <DragIcon type="primary" />
-                <ConstructorElement text={el.name} price={el.price} thumbnail={el.image} />
-              </div>
+            {ingredientList.map((el, index) => (
+              <DraggableItem
+                moveItem={moveItem}
+                key={uuid()}
+                id={uuid()}
+                index={index}
+                text={el.name}
+                price={el.price}
+                thumbnail={el.image}
+              />
             ))}
           </div>
           <div className="pl-4 pr-4">
---
+++ src/components/burger-constructor/draggable-item.tsx
@@ -0,0 +1,89 @@
+import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import type { Identifier, XYCoord } from 'dnd-core';
+import { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+import styles from './style.module.css';
+
+interface IProps {
+  id: any;
+  index: number;
+  text: string;
+  price: number;
+  thumbnail: string;
+  moveItem: (dragIndex: number, hoverIndex: number) => void;
+}
+
+interface DragItem {
+  index: number;
+  id: string;
+  type: string;
+}
+
+export const DraggableItem = ({
+  id,
+  text,
+  index,
+  price,
+  moveItem,
+  thumbnail,
+  ...props
+}: IProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [{ handlerId }, drop] = useDrop<DragItem, void, { handlerId: Identifier | null }>({
+    accept: 'sortItem',
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item: DragItem, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+      moveItem(dragIndex, hoverIndex);
+
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: 'sortItem',
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor: any) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+  console.log(isDragging);
+
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
+  return (
+    <div
+      ref={ref}
+      style={{ opacity }}
+      data-handler-id={handlerId}
+      key={id}
+      className={styles.availableConstructorItem}>
+      <DragIcon type="primary" />
+      <ConstructorElement text={text} price={price} thumbnail={thumbnail} />
+    </div>
+  );
+};
