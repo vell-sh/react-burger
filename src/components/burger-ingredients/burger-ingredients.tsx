5c41fcdee5bcfc4b8685df82adb631fc7ecdfc88
Valeriya <val.shelukhina@gmail.com>
2023-03-26 14:54:54 +0300

    add scroll for BurgerIngredients

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,6 +1,6 @@
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import cn from 'classnames';
-import React from 'react';
+import React, { useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { getRandomRangeValue } from '../../lib/utils';
 import { formatIngredientType } from '../../services/format.service';
@@ -18,15 +18,15 @@ interface IProps {
 }
 
 const BurgerIngredients = ({ className }: IProps) => {
-  const [current, setCurrent] = React.useState('bun');
+  const [current, setCurrent] = React.useState(IngredientType.bun as string);
   const [currentIngredient, setCurrentIngredient] = React.useState<IIngredient | null>(null);
   const [isVisible, setIsVisible] = React.useState(false);
 
   const ingredients = useSelector((store: RootState) => store.ingredients.items);
 
-  const bunItems = ingredients.filter(x => x.type === 'bun');
-  const sauceItems = ingredients.filter(x => x.type === 'sauce');
-  const mainItems = ingredients.filter(x => x.type === 'main');
+  const bunItems = ingredients.filter(x => x.type === IngredientType.bun);
+  const sauceItems = ingredients.filter(x => x.type === IngredientType.sauce);
+  const mainItems = ingredients.filter(x => x.type === IngredientType.main);
 
   const handleOpenModal = (ingredient: IIngredient) => {
     setCurrentIngredient(ingredient);
@@ -37,31 +37,61 @@ const BurgerIngredients = ({ className }: IProps) => {
     setIsVisible(false);
   };
 
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const bunRef = useRef<HTMLDivElement>(null);
+  const sauceRef = useRef<HTMLDivElement>(null);
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  const getRefFromIngredientType = (type: IngredientType) => {
+    switch (type) {
+      case IngredientType.sauce:
+        return sauceRef;
+      case IngredientType.main:
+        return mainRef;
+      case IngredientType.bun:
+      default:
+        return bunRef;
+    }
+  };
+
+  const onScroll = (to: IngredientType) => {
+    setCurrent(to);
+    const ref = getRefFromIngredientType(to);
+    console.log(to);
+    console.log(ref.current);
+    console.log(current);
+    if (ref.current) {
+      ref.current.scrollIntoView({ block: 'start', behavior: 'smooth' });
+    }
+  };
+
   return (
     <section className={className}>
       <div className={styles.menu}>
         <Tab
           value={IngredientType.bun}
           active={current === IngredientType.bun}
-          onClick={setCurrent}>
+          onClick={() => onScroll(IngredientType.bun)}>
           {formatIngredientType(IngredientType.bun)}
         </Tab>
         <Tab
           value={IngredientType.sauce}
           active={current === IngredientType.sauce}
-          onClick={setCurrent}>
+          onClick={() => onScroll(IngredientType.sauce)}>
           {formatIngredientType(IngredientType.sauce)}
         </Tab>
         <Tab
           value={IngredientType.main}
           active={current === IngredientType.main}
-          onClick={setCurrent}>
+          onClick={() => onScroll(IngredientType.main)}>
           {formatIngredientType(IngredientType.main)}
         </Tab>
       </div>
-      <div className={cn('custom-scroll', styles.container)}>
+      <div className={cn('custom-scroll', styles.container)} ref={scrollContainerRef}>
         <div>
-          <BurgerIngredientsWrapper title={formatIngredientType(IngredientType.bun)}>
+          <BurgerIngredientsWrapper
+            title={formatIngredientType(IngredientType.bun)}
+            innerRef={bunRef}>
             {bunItems.map(x => (
               <BurgerIngredient
                 key={x._id}
@@ -71,12 +101,16 @@ const BurgerIngredients = ({ className }: IProps) => {
               />
             ))}
           </BurgerIngredientsWrapper>
-          <BurgerIngredientsWrapper title={formatIngredientType(IngredientType.sauce)}>
+          <BurgerIngredientsWrapper
+            title={formatIngredientType(IngredientType.sauce)}
+            innerRef={sauceRef}>
             {sauceItems.map(x => (
               <BurgerIngredient key={x._id} item={x} onClick={handleOpenModal} />
             ))}
           </BurgerIngredientsWrapper>
-          <BurgerIngredientsWrapper title={formatIngredientType(IngredientType.main)}>
+          <BurgerIngredientsWrapper
+            title={formatIngredientType(IngredientType.main)}
+            innerRef={mainRef}>
             {mainItems.map(x => (
               <BurgerIngredient key={x._id} item={x} onClick={handleOpenModal} />
             ))}
