ec7aeb53e05a1b6327a893abbae5ccd3670b998c
Valeriya <funny_imlay@list.ru>
2023-04-14 03:40:15 +0300

    improve NotFoundPage

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,5 +1,14 @@
+import cn from 'classnames';
+import styles from './styles.module.css';
+import logo from '../../images/logo.svg';
+
 const NotFoundPage = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className={styles.main}>
+      <h1 className={cn('text text_type_main-large pt-30 mb-10', styles.title)}>Страница не найдена</h1>
+      <img className={styles.image} src={logo} alt="Логотип" />
+    </div>
+  );
 };
 
 export default NotFoundPage;
